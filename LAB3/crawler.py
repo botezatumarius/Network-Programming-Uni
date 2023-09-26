8c7dac02866cdc664ffb2b5722ab7d63b2b74347
botezatumarius <ufobelievers27@gmail.com>
2023-09-26 23:06:17 +0300

    Lab3 classwork

---
+++ LAB3/crawler.py
@@ -0,0 +1,38 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+os.system('clear')
+
+
+def urlExtracter(url, urls, carNames, pageNumber, lastPage="null"):
+    print('page ', pageNumber)
+    reqs = requests.get(url+f"?page={pageNumber}")
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+
+    cars = soup.find_all('div', class_='ads-list-photo-item-title')
+    if lastPage == "null":
+        lastPage = soup.find('li', class_='is-last-page')
+        lastPageNumber = lastPage.find('a').get(
+            'href').replace("/ro/list/transport/cars?page=", "")
+        lastPage = int(lastPageNumber)
+
+    for car in cars:
+        title = car.find('a').text.replace(' ', '')
+        link = car.find('a').get('href')
+        if ("booster" not in link) and ("recommendations" not in link):
+            carNames.append(title)
+            urls.append("https://999.md"+link)
+            print("https://999.md"+link)
+
+    pageNumber += 1
+    if pageNumber <= lastPage:
+        urlExtracter(url, urls, carNames, pageNumber, lastPage)
+
+
+url = 'https://999.md/ro/list/transport/cars'
+urls = []
+carNames = []
+
+urlExtracter(url, urls, carNames, 1)
+# print(urls)
